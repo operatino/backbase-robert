d47125553f09e980f5a1e3f020903d04c913855c
Robert Haritonov <r@rhr.me>
2015-06-05 16:35:50 +0200

    add stand-alone widget example

---
+++ standalone-widgets/robert-feed/js/robert-feed.js
@@ -0,0 +1,45 @@
+function Feed(widget) {
+	this.widget = widget;
+	this.limitPref = 'limit';
+
+	// Status log
+	this.widget.model.addEventListener('PrefModified', function (event) {
+		console.log('PrefModified', event);
+	});
+}
+
+Feed.prototype.init = function(){
+	this.$el = $('.rb-robert-feed');
+
+	this.initPrefs();
+	this.drawNews();
+};
+
+Feed.prototype.initPrefs = function(){
+	var limit = parseFloat(this.widget.model.getPreference(this.limitPref));
+	this.limit = Number.isInteger(limit) ? limit : 5;
+};
+
+Feed.prototype.drawNews = function(){
+	var _this = this;
+	var tpl = document.getElementById('rb-robert-feed_list').innerHTML;
+
+	$.ajax({
+		type: 'GET',
+		data: {
+			url: 'http://podcasts.engadget.com/rss.xml'
+		},
+		url: '/portalserver/proxy?pipe=jsonPipe'
+	}).done(function (response) {
+		var rssJson = response;
+		var newsList = rssJson.channel.item;
+
+		newsList = newsList.slice(0, _this.limit);
+
+		_this.$el.append(mst.render(tpl, {
+			list: newsList
+		}));
+	});
+};
+
+window.Feed = Feed;
